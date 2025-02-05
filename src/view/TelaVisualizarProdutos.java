59b96681693f5af5915f72f37028205072f39833
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-02-05 08:25:11 -0300

    Criando método para montar a tabela

--- src/view/TelaVisualizarProdutos.java
+++ src/view/TelaVisualizarProdutos.java
@@ -4,7 +4,9 @@ import dao.ProdutoDao;
 import model.Produto;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.io.IOException;
+import java.util.List;
 import java.util.Set;
 
 public class TelaVisualizarProdutos extends JDialog {
@@ -20,12 +22,26 @@ public class TelaVisualizarProdutos extends JDialog {
 
     private void createUIComponents() {
         ProdutoDao dao = new ProdutoDao();
+        Set<Produto> produtos = null;
         try {
-            Set<Produto> produtos = dao.getProdutos();
+            produtos = dao.getProdutos();
         } catch (IOException e) {
             throw new RuntimeException(e);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
+        }
+        if(produtos!=null){
+            table1 = new JTable();
+            String titulos[] = {"Código", "Descrição", "Preço", "Validade"};
+            String conteudo[][] = new String[produtos.size()][4];
+            List<Produto> produtoList = produtos.stream().toList();
+            for(int i=0; i<produtoList.size(); i++){
+                conteudo[i][0] = ""+produtoList.get(i).getId();
+                conteudo[i][1] = produtoList.get(i).getDescricao();
+                conteudo[i][2] = ""+produtoList.get(i).getPreco();
+                conteudo[i][3] = ""+produtoList.get(i).getValidade();
+            }
+            table1.setModel(new DefaultTableModel(conteudo, titulos));
         }
     }
 }

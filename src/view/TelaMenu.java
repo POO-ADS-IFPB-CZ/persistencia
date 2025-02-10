f6922162bbb33b0c2934f3d92276758a818c8540
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-02-10 10:36:58 -0300

    adicionando exemplo de evento de mouse

--- src/view/TelaMenu.java
+++ src/view/TelaMenu.java
@@ -3,6 +3,8 @@ package view;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class TelaMenu extends JFrame {
     private JPanel contentPane;
@@ -41,6 +43,12 @@ public class TelaMenu extends JFrame {
                 cadastroCliente.setLocationRelativeTo(null);
                 cadastroCliente.setVisible(true);
                 setVisible(true);
+            }
+        });
+        vendasButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                System.out.println("Mouse entrou "+e.getX()+","+e.getY());
             }
         });
     }

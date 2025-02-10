112e4f28d9219b118362705c68bb1ada35af59b7
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-02-10 09:24:52 -0300

    Adicionando funcionalidade de remover

--- src/view/TelaCadastroProduto.java
+++ src/view/TelaCadastroProduto.java
@@ -87,6 +87,27 @@ public class TelaCadastroProduto extends JDialog {
                 visualizarProdutos.setVisible(true);
             }
         });
+        buttonCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String codigoString = campoCodigo.getText();
+                int codigo = Integer.parseInt(codigoString);
+                Produto produto = new Produto(codigo, null,
+                        0, null);
+                try {
+                    if(produtoDao.removerProduto(produto)){
+                        JOptionPane.showMessageDialog(null,
+                                "Removido com sucesso");
+                    }else{
+                        JOptionPane.showMessageDialog(null,
+                                "Código não existe");
+                    }
+                } catch (IOException | ClassNotFoundException ex) {
+                    JOptionPane.showMessageDialog(null,
+                            "Falha na conexão com o arquivo");
+                }
+            }
+        });
     }
 
     private void createUIComponents() {

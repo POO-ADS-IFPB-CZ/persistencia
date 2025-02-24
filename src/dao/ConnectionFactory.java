2f6bca5a7162afa1e96663b603e470977beaabcb
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-02-24 10:16:14 -0300

    Removendo parte do teste da conexão

--- src/dao/ConnectionFactory.java
+++ src/dao/ConnectionFactory.java
@@ -9,7 +9,6 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionFactory {
-
     public Connection getConnection() throws IOException,
             ClassNotFoundException, SQLException {
         Properties prop = new Properties();
@@ -17,22 +16,4 @@ public class ConnectionFactory {
         Class.forName("org.postgresql.Driver");
         return DriverManager.getConnection(prop.getProperty("url"));
     }
-
-    public static void main(String[] args){
-        ConnectionFactory cf = new ConnectionFactory();
-        try {
-            if(cf.getConnection() != null){
-                System.out.println("Conectado");
-            }else{
-                System.out.println("Falha");
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
 }

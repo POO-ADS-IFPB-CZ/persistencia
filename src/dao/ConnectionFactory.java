1304423b8f49f56a3c8c8d2f4d05a8c9b7dde9f4
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-02-24 09:59:43 -0300

    Adicionando teste da conexão

--- src/dao/ConnectionFactory.java
+++ src/dao/ConnectionFactory.java
@@ -4,22 +4,33 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionFactory {
 
-    public Connection getConnection() throws IOException {
+    public Connection getConnection() throws IOException,
+            ClassNotFoundException, SQLException {
         Properties prop = new Properties();
         prop.load(new FileReader("database.properties"));
-        System.out.println(prop.getProperty("url"));
-        return null;
+        Class.forName("org.postgresql.Driver");
+        return DriverManager.getConnection(prop.getProperty("url"));
     }
 
     public static void main(String[] args){
         ConnectionFactory cf = new ConnectionFactory();
         try {
-            cf.getConnection();
+            if(cf.getConnection() != null){
+                System.out.println("Conectado");
+            }else{
+                System.out.println("Falha");
+            }
         } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
